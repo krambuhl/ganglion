4e123d58f40fc824b884f7897a6560df9ee00ed0
Evan Krambuhl <thekrambuhl@gmail.com>
2015-01-21 17:49:01 -0800

    sockets

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-module.exports = function() {
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+var MsgBank = require('message-bank');
+var bank = new MsgBank();
 
-};
+io.on('connection', function(socket){
+  var id = bank.subscribe('CONTACTS', function(d, o) {
+    socket.emit('data', { type: 'CONTACTS', data: d, options: o });
+  });
+
+  socket.on('disconnect', function(){
+    bank.unsubscribe(id);
+  });
+});
+
+var i = 0;
+setInterval(function() {
+  bank.dispatch({
+    type: 'CONTACTS',
+    data: {
+      dogs: !!(i++)
+    }
+  });
+}, 5000 * Math.random());
+
+
+server.listen(4201);
