746a969cd3ca36a3bad5c0b8ed91feb87a04968e
Evan Krambuhl <thekrambuhl@gmail.com>
2015-01-23 01:14:58 -0800

    minor message cache before DB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var server = require('http').createServer();
 var io = require('socket.io')(server);
+var fs = require('fs');
 
 var bank = require('./lib/bank.js');
 
@@ -13,16 +14,40 @@ io.on('connection', function(socket){
   });
 });
 
+var cache;
+fs.readFile('./cache/message-box.json', 'utf8', function (err, data) {
+  if (!err) {
+    cache = JSON.parse(data);
+    bank.dispatch(cache);
+  }
+});
+
+bank.subscribe(function(d, o, t) {
+  var sum = [];
+  for (var row in bank._store) {
+    sum.push({
+      type: row,
+      data: bank._store[row].data,
+      options: bank._store[row].options
+    });
+  }
+
+  fs.writeFile('./cache/message-box.json', JSON.stringify(sum), function (err) {
+    if (err) return console.log(err);
+  });
+});
+
+
 var i = 0;
 setInterval(function() {
   bank.dispatch({
-    type: 'CONTACTS',
+    type: 'TWITTER',
     data: {
       timestamp: new Date(),
       dogs: Boolean((i++) % 2)
     }
   });
-}, 5 * 1000);
+}, 10 * 1000);
 
 
 server.listen(4201);
