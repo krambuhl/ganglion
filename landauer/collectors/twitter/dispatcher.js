1edaece11e5033e791475a10058b37891ca791c8
Evan Krambuhl <thekrambuhl@gmail.com>
2015-01-25 23:47:39 -0800

    splitting off to collection/auth server and interface server

---
+++ landauer/collectors/twitter/dispatcher.js
@@ -0,0 +1,11 @@
+var bank = require('message-bank');
+
+setInterval(function() {
+  bank.dispatch({
+    type: 'TWITTER',
+    data: {
+      timestamp: new Date(),
+      dogs: Boolean(Math.round(Math.random()))
+    }
+  });
+}, 10 * 1000);
--- tests/index.test.js
+++
@@ -1,7 +0,0 @@
-var bank = require('../');
-var test = require('tape');
-
-test('simple comparisons', function (t) {
-  t.plan(1);
-  t.equal(1, 1);
-});
